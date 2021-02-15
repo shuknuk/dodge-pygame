66d093b6f023735e45d012341342c1151083af22
Kinshuk-Goel <kg467@outlook.com>
2021-02-14 19:35:25 -0500

    Another commit made to the project. Inspect more for details.

--- program.py
+++ program.py
@@ -1,30 +1,56 @@
-# Main point of creating a simple python program.
-
-# 1. First, to import the pygame library # This code will also initialize the pygame library
+# Import module - pygame
 import pygame
+
+# Import pygame.locals | Detect hardware input from user:
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Initialize pygame
 pygame.init()
 
-# Size of the window
-screen = pygame.display.set_mode([500, 500])
+# Define screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-# Having the game be running until suspended by user:
+# Create screen object | Determined by width+height of screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Keep loop running, variable config:
 running = True
-while running:
 
-    # Making value X-button = close_window_button
+# Script of loop:
+while running:
+    # Look at every event in the queue
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # Detect input of key from user
+        if event.type == KEYDOWN:
+            # If key having to be esc, suspend program:
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Manual suspending program using X-button
+        elif event.type == QUIT:
             running = False
 
-    # Gray background! | Fill background with a gray color. # Gray color has number 200. 
-    screen.fill((200, 200, 200))
+# Screen with white (imma prob. change it to another color later, idk)
+screen.fill((255, 255, 255))
 
-    # Blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+# Surface length and width
+surf = pygame.Surface((50, 50))
 
-    # Flip the display
-    pygame.display.flip()
+# Surface be separate from background
+surf.fill((0, 0, 0))
+rect = surf.get_rect()
 
-# Script complete! If having to quit the program:
-pygame.quit()
-# Creating another label. Make sure it is seen!
+# The following commands below this will use .blit() and .flip()
+# Note, .blit() stands for Block Transfer
+# This line says: "Draw surf onto screen at center"
+screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+pygame.display.flip()
